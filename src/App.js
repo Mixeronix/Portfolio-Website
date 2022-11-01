764e9fd059a872bec53fd309d2f5e4c3686352ac
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2022-11-01 16:53:07 +0100

    New website

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import mac from "./images/mac.png";
+import "./app.css";
+
+export default function App() {
+	return (
+		<div className="hero">
+			<img src={mac} alt="" className="mac" />
+		</div>
+	);
+}
