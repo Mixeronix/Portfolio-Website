7d6e8af384e9a6edc7f83278e73c64e5f6b1fcf6
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-12 15:33:44 +0200

    Design changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ const openSansFont = Open_Sans({
 
 export default function Home() {
 	return (
-		<main>
-			<div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 ">
-				<h1 className={"text-9xl text-center " + avenirFont.className}>Bartosz Wiaderek</h1>
-				<h2 className={"text-3xl text-center mt-3 " + openSansFont.className}>Website available later...</h2>
+		<main className="w-screen h-screen bg-background">
+			<div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 w-full text-gray ">
+				<h1 className={"text-7xl sm:text-8xl md:text-9xl text-center " + avenirFont.className}>Bartosz Wiaderek</h1>
+				<h2 className={"text-1xl sm:text-2xl md:text-3xl text-center mt-3 " + openSansFont.className}>Website available later...</h2>
 			</div>
 		</main>
 	);
