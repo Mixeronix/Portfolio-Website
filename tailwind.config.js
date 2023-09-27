9e6e7eebdb50149fb1482879dc5d05f48f52b67d
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-09-27 08:28:11 +0200

    First design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				Black: "#291F1E",
+				White: "#FFFFFF",
+				Green: "#5EEB5B",
 			},
-			colors: { background: "#EEEDE8", gray: "#222222" },
 			fontSize: {
 				"14xl": "13rem",
 			},
