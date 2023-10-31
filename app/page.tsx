7af8dbba6b3b092132bf0c9a37b2ac1e1965a805
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-10-31 07:56:24 +0100

    Framer animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import localFont from "next/font/local";
+"use client";
 
+import localFont from "next/font/local";
 const valenciaFont = localFont({ src: "./Valencia.ttf" });
 import { Open_Sans } from "next/font/google";
+import { motion } from "framer-motion";
 
 const openSansFont = Open_Sans({
 	weight: "600",
@@ -10,11 +12,36 @@ const openSansFont = Open_Sans({
 
 export default function Home() {
 	return (
-		<main className="w-screen h-screen bg-background">
-			<div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 w-full text-gray ">
-				<h1 className={"text-7xl sm:text-8xl md:text-9xl text-center lg:text-14xl " + valenciaFont.className}>Bartosz Wiaderek</h1>
-				<h2 className={"text-1xl sm:text-2xl md:text-3xl text-center mt-6 uppercase " + openSansFont.className}>Website available later...</h2>
-			</div>
+		<main className="w-full flex flex-col gap-x-10 bg-background">
+			<section className="w-screen h-screen">
+				<div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 w-full text-gray ">
+					<div className="overflow-hidden ">
+						<motion.h1
+							initial={{ y: "100%" }}
+							animate={{ y: 0 }}
+							transition={{ duration: 0.5 }}
+							className={"text-7xl sm:text-8xl md:text-9xl z-10 h-fit text-center lg:text-14xl " + valenciaFont.className}
+						>
+							Bartosz Wiaderek
+						</motion.h1>
+					</div>
+					<div className="overflow-hidden ">
+						<motion.h2
+							initial={{ opacity: 0, y: "100%" }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{
+								type: "spring",
+								stiffness: 260,
+								damping: 20,
+								delay: 0.45,
+							}}
+							className={"text-1xl sm:text-2xl md:text-3xl text-center uppercase text-gray/70 " + openSansFont.className}
+						>
+							Website available later...
+						</motion.h2>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 }
